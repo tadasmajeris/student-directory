91cf2f5c23262b8f651a1b67fad63250efbf6cde
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-13 15:03:20 +0100

    Exercise 1. each_with_index

--- directory.rb
+++ directory.rb
@@ -18,8 +18,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, i|
+    puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
