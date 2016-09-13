c38524910526e1b8f485b3bbabecf04601b720b7
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-13 14:34:16 +0100

    refactoring.. extracted code to methods

--- directory.rb
+++ directory.rb
@@ -12,10 +12,19 @@ students = [
   "Norman Bates"
 ]
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-puts "The students of Villains Academy"
-puts "-------------"
+def print(names)
+  names.each { |name| puts name }
+end
 
-students.each { |student| puts student }
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
-puts "Overall, we have #{students.count} great students"
+print_header
+print(students)
+print_footer(students)
