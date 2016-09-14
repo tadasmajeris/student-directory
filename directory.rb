9e7d59fe2ef558618c9d0e657c7b38791217cd0c
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-14 11:41:19 +0100

    Exercise 6. Used .center() to print a table. Some refactoring

--- directory.rb
+++ directory.rb
@@ -2,11 +2,11 @@
 #   {name: "Dr. Hannibal Lecter", cohort: :november,
 #     hobbies: ['puzzles', 'eating humans'],
 #     country: 'America', height: 1.7 },
-#   {name: "Darth Vader", cohort: :november, hobbies: ['conquering the universe', 'red lightsabers'], height: 2},
+#   {name: "Darth Vader", cohort: :november, hobbies: ['universe', 'red lightsabers'], height: 2},
 #   {name: "Nurse Ratched", cohort: :november},
 #   {name: "Michael Corleone", cohort: :november},
 #   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november, country: "Lithuania"},
 #   {name: "Terminator", cohort: :november},
 #   {name: "Freddy Krueger", cohort: :november},
 #   {name: "The Joker", cohort: :november},
@@ -15,11 +15,45 @@
 # ]
 
 def print_header
+  puts
   puts "The students of Villains Academy"
-  puts "-------------"
+  print_line
 end
 
-def print(students, args = {})
+def print_line(n=90)
+  puts '-' * n
+end
+
+def print_table_header(students, max_chars)
+  if students.count > 0
+    $stdout.print 'No. '
+    students.first.keys.each do |key|
+      $stdout.print key.to_s.center(max_chars[key])
+    end
+    puts
+    print_line
+  end
+end
+
+# Find a maximum character count in students names, hobies, etc.
+# for printing a flexible table of students data.
+def find_max_chars(students)
+  max_chars = {}
+  students.each do |student|
+    student.each do |k, v|
+      key_length = k.to_s.length
+      value_length = ((v.is_a? Array) ? v.join(', ') : v.to_s).length
+      current_max = (key_length > value_length) ? key_length : value_length + 2
+      if (max_chars[k] == nil) || (current_max > max_chars[k])
+        max_chars[k] = current_max
+      end
+    end
+  end
+  max_chars
+end
+
+# selecting students by given arguments (first letter, name length)
+def select_by_arguments(students, args)
   first_letter = args[:first_letter]
   name_length = args[:name_length]
   if first_letter
@@ -32,28 +66,38 @@ def print(students, args = {})
       student[:name].length < name_length
     end
   end
+  students
+end
+
+def print(students, args = {})
+
+  students  = select_by_arguments(students, args)
+  max_chars = find_max_chars(students)
+  print_table_header(students, max_chars)
 
   i = 0
   while students.count > 0
     student = students.shift
     i += 1
-    puts "#{i}. #{student[:name]}"
+    $stdout.print "#{i}. ".ljust(4)
     # print students hobbies, country, height, etc
-    student.each do |k, v|
-      if (k != :name) && (v.to_s != '')
-        puts "#{k}: #{(v.is_a? Array) ? v.join(', ') : v}"
-      end
+    max_chars.keys.each do |k|
+      v = student[k]
+      $stdout.print (v.is_a?(Array)? v.join(', ') : v).to_s.center(max_chars[k])
     end
     puts
   end
+  print_line
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
+  puts
 end
 
 def input_students
   students = []
+  puts
   puts "Enter the name of student No.1"
   name = gets.chomp
 
@@ -85,7 +129,12 @@ def input_students
   students
 end
 
+######## PROGRAM ########
 students = input_students
-print_header
-print(students, {name_length: 30})
-print_footer(students)
+if students.count > 0
+  print_header
+  print(students, {name_length: 30})
+  print_footer(students)
+else
+  puts "No students..\n\n"
+end
