e6f26d74dce1498534a37798840a18e177a05436
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-14 14:05:21 +0100

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -147,12 +147,34 @@ def input_students
   students
 end
 
-######## PROGRAM ########
-students = input_students
-if students.count > 0 # dont print empty list
-  print_header
-  print(students, {name_length: 30})
-  print_footer(students)
-else
-  puts "No students..\n\n"
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp.strip
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      if students.count > 0 # dont print empty list
+        print_header
+        print(students, {name_length: 30})
+        print_footer(students)
+      else
+        puts "No students..\n\n"
+      end
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
 end
+
+######## PROGRAM ########
+interactive_menu
